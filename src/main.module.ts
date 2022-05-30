c62691c89ce0f0dd340e548ffe15378a53878262
Frorloski <fernando.orloski@platformbuilders.com>
2022-05-30 19:01:14 -0300

    feat: added helmet, cors and throttle

--- src/main.module.ts
+++ src/main.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { ProvidersModule } from './providers/providers.module';
 import { ResourcesModule } from './resources/resources.module';
 
 @Module({
-  imports: [ProvidersModule, ResourcesModule],
+  imports: [
+    ProvidersModule,
+    ResourcesModule,
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 60,
+    }),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class MainModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { MainModule } from './main.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
+  app.use(helmet());
+  app.enableCors();
   await app.listen(3002);
 }
 bootstrap();
